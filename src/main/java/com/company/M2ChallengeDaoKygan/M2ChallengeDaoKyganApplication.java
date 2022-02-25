c4c5ab12ab73b2a86fe4357b8ec857f5c49b7e3d
Kygan <kygandao23@gmail.com>
2022-02-25 13:30:27 -0800

    initial commit

---
+++ src/main/java/com/company/M2ChallengeDaoKygan/M2ChallengeDaoKyganApplication.java
@@ -0,0 +1,13 @@
+package com.company.M2ChallengeDaoKygan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2ChallengeDaoKyganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2ChallengeDaoKyganApplication.class, args);
+	}
+
+}
